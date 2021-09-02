b03985ff7a34eddae4c255dcbb6034f34d7cb9f7
Nathan Nguyen <nathanknguyen96@gmail.com>
2021-09-01 18:28:17 -0700

    pulling out all html elements

---
+++ assets/script.js
@@ -0,0 +1,56 @@
+//Pulling over elements from html
+//section here has elements from left bar of website
+var citySearchBar = document.querySelector("#citySearchBar");
+var citySearchBarButton = document.querySelector("#citySearchBarButton");
+var AustinButton = document.querySelector("#AustinButton");
+var ChicagoButton = document.querySelector("#ChicagoButton");
+var NewYorkButton = document.querySelector("#NewYorkButton");
+var OrlandoButton = document.querySelector("#OrlandoButton");
+var SanFranciscoButton = document.querySelector("#SanFranciscoButton");
+var SeattleButton = document.querySelector("#SeattleButton");
+var DenverButton = document.querySelector("#DenverButton");
+var AtlantaButton = document.querySelector("#AtlantaButton");
+var currentCity = document.querySelector("#currentCity");
+var currentCityDate = document.querySelector("#currentCityDate");
+var currentCityWeather = document.querySelector("#currentCityWeather")
+var currentCityTemp = document.querySelector("#currentCityTemp");
+var currentCityWind = document.querySelector("#currentCityWind");
+var currentCityHumidity = document.querySelector("#currentCityHumidity");
+var currentCityUVI = document.querySelector("#currentCityUVI");
+
+//html elements for next day of selected city
+var day1 = document.querySelector("#day1");
+var day1date = document.querySelector("#day1date")
+var day1weather = document.querySelector("#day1weather");
+var day1temp = document.querySelector("#day1temp");
+var day1wind = document.querySelector("#day1wind");
+var day1humidity = document.querySelector("#day1humidity");
+
+var day2 = document.querySelector("#day2");
+var day2date = document.querySelector("#day2date")
+var day2weather = document.querySelector("#day2weather");
+var day2temp = document.querySelector("#day2temp");
+var day2wind = document.querySelector("#day2wind");
+var day2humidity = document.querySelector("#day2humidity");
+
+var day3 = document.querySelector("#day3");
+var day3date = document.querySelector("#day3date")
+var day3weather = document.querySelector("#day3weather");
+var day3temp = document.querySelector("#day3temp");
+var day3wind = document.querySelector("#day3wind");
+var day3humidity = document.querySelector("#day3humidity");
+
+var day4 = document.querySelector("#day4");
+var day4date = document.querySelector("#day4date")
+var day4weather = document.querySelector("#day4weather");
+var day4temp = document.querySelector("#day4temp");
+var day4wind = document.querySelector("#day4wind");
+var day4humidity = document.querySelector("#day4humidity");
+
+var day5 = document.querySelector("#day5");
+var day5date = document.querySelector("#day5date")
+var day5weather = document.querySelector("#day5weather");
+var day5temp = document.querySelector("#day5temp");
+var day5wind = document.querySelector("#day5wind");
+var day5humidity = document.querySelector("#day5humidity");
+
